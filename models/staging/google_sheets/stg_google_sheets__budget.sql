c4fa4ee54ee7dd3272a76ed97332ca464e0d80ee
Gerardo Guerrero Felix <jgguerrero.felix@gmail.com>
2025-05-08 15:49:15 +0000

    modelo_prueba

---
+++ models/staging/google_sheets/stg_google_sheets__budget.sql
@@ -0,0 +1,22 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+WITH src_budget AS (
+    SELECT * 
+    FROM {{ source('google_sheets', 'budget') }}
+    ),
+
+renamed_casted AS (
+    SELECT
+        {{dbt_utils.generate_surrogate_key(['product_id', 'month'])}} AS budget_id
+        , product_id
+        , quantity
+        , month AS monthly_date
+        , _fivetran_synced
+    FROM src_budget
+    )
+
+SELECT * FROM renamed_casted
